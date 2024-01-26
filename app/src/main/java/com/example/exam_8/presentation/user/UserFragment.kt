ef9a597613742e40c5a041ac4c8f5b9605fefce2
Davitpurceladze <purceladzedavit@gmail.com>
2024-01-26 11:23:30 +0400

    Exam_8

---
+++ app/src/main/java/com/example/exam_8/presentation/user/UserFragment.kt
@@ -0,0 +1,16 @@
+package com.example.exam_8.presentation.user
+
+import com.example.exam_8.databinding.FragmentUserBinding
+import com.example.exam_8.presentation.base.BaseFragment
+
+
+class UserFragment : BaseFragment<FragmentUserBinding>(FragmentUserBinding::inflate) {
+    override fun bind() {
+    }
+
+    override fun bindViewActionListeners() {
+    }
+
+    override fun bindObserves() {
+    }
+}
